cd2a8b8b3400de9149c29f40d229d141ee2a517c
Boris Ts <b.tsankov@student.fontys.nl>
2025-04-08 19:27:20 +0200

    small changes to goal model

--- MealPulse/Models/Models/Goal.cs
+++ MealPulse/Models/Models/Goal.cs
@@ -6,21 +6,21 @@ namespace MealPulse.Models.Models
     public class Goal
     {
         [Key]
-        public int GoalId { get; set; }
+        public int goal_id { get; set; }
 
         [ForeignKey(nameof(User))]
-        public int UserId { get; set; }
+        public int user_id { get; set; }
 
         [Range(WeightMin, WeightMax)]
-        public double CurrentWeightKg { get; set; }
+        public double current_weight_kg { get; set; }
 
         [Range(WeightMin, WeightMax)]
-        public double TargetWeightKg { get; set; }
+        public double target_weight_kg { get; set; }
 
-        public bool IsActive { get; set; }
+        public bool is_active { get; set; }
 
-        public DateTime StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime? end_date { get; set; }
 
         public User User { get; set; } = null!;
         public ICollection<FoodDiaryItem> FoodDiaryItems { get; set; } = new List<FoodDiaryItem>();
